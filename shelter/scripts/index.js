13f1ae3ca3dfc804c97e4f14093a549115a1d5a8
kozhemyakin <pctdoc@gmail.com>
2023-04-08 15:14:53 +0300

    refactoring: slider main page

--- shelter/scripts/index.js
+++ shelter/scripts/index.js
@@ -88,8 +88,76 @@ const data = [
     "inoculations": ["bordetella bronchiseptica", "leptospirosis"],
     "diseases": ["deafness", "blindness"],
     "parasites": ["lice", "fleas"]
-    }
+    },
 ]
+
+function chunk(arr, chunkSize) {
+const clone = [...arr]
+if (chunkSize <= 0) throw "Invalid chunk size";
+var R = [];
+for (var i=0,len=clone.length; i<len; i+=chunkSize)
+    R.push(clone.slice(i,i+chunkSize));
+return R;
+}
+
+const shuffle = (arr) => {
+    return arr.sort(() => Math.round(Math.random() * 100) - 50);
+}
+
+const shuffledData = shuffle(data);
+
+function addElement(parrent, image, name) {
+    const card = document.createElement("div");
+    const cardImage = document.createElement("div")
+    const button = document.createElement("button");
+    const paragraph = document.createElement("p");
+
+    card.className = 'our-friends__slider__card';
+    
+    card.setAttribute('data-name', name);
+    cardImage.className = 'our-friends__slider__card--image';
+    paragraph.className = 'our-friends__slider__card--name';
+    button.className = 'our-friends__slider__card--button';
+  
+    cardImage.style.backgroundImage = `url('${image}')`;
+    cardImage.style.height = '270px';
+    cardImage.style.width = '270px';
+  
+    const petName = document.createTextNode(name);
+    const buttonText = document.createTextNode('Learn more');
+    
+    paragraph.appendChild(petName);
+    button.appendChild(buttonText);
+
+
+    parrent.append(card);
+    card.append(cardImage)
+    card.append(paragraph)
+    card.append(button)
+}
+
+function addSlide(slide) {
+    const slider = document.querySelector('.our-friends__slider__cards')
+
+
+    const slideWrapper = document.createElement("li")
+
+    slideWrapper.className = 'slide animate';
+
+    slide.forEach((card) => {
+        addElement(slideWrapper, card.img, card.name)
+    })
+
+    slider.appendChild(slideWrapper)
+
+}
+
+
+// const petCard = shuffledData.map((pet) => {
+//     const cardsDiv = document.querySelector('.our-friends__slider__cards');
+
+//     addElement(cardsDiv, pet.img, pet.name)
+// })
 
 const burgerToggle = document.getElementById('menu__toggle');
 const burgerMenu = document.querySelector('.hamburger-menu');
@@ -125,9 +193,6 @@ cards.forEach((el) => {
     el.addEventListener('click', function (e) {
         document.body.classList.toggle('overflow-hidden-class');
 
-        // let petName = this.dataset.name;
-        // console.log(petName)
-        console.log(this.dataset.name)
         modalWindow.style.display = 'flex';
         modalWindowBg.style.display = 'block';
 
@@ -140,10 +205,6 @@ cards.forEach((el) => {
         document.querySelector('.modal-window__content--description--list--diseases').textContent = petValues.diseases;
         document.querySelector('.modal-window__content--description--list--parasites').textContent = petValues.parasites;
         document.querySelector('.modal-window__content--image').style.backgroundImage = `url('${petValues.img}')`;
-        
-
-        
-        console.log(petValues)
     })
 })
 
@@ -161,26 +222,155 @@ krestik.addEventListener('click', function () {
     modalWindowBg.style.display = 'none';
 })
 
+/* sdfjhdsjkhfkhjdsfkjhdsjkhfhjskdfhkdsf */
 
-// sozdat karto4ki i modalku 4erez JS
+var index = 0,
+    amount = 0,
+    currTransl = [],
+    translationComplete = true,
+    moveOffset = 0;
 
-document.body.onload = addCards;
+var transitionCompleted = function(){
+    translationComplete = true;
+}
 
-const petCard = data.map((pet) => {
-    document.body.onload = addElement;
-})
+const calculateSliderPerPage = (width) => {
 
-function addCards() {
-    // create a new div element
-    const newDiv = document.createElement("div");
-  
-    // and give it some content
-    const newContent = document.createTextNode("Hi there and greetings!");
-  
-    // add the text node to the newly created div
-    newDiv.appendChild(newContent);
-  
-    // add the newly created element and its content into the DOM
-    const currentDiv = document.getElementById("div1");
-    document.body.insertBefore(newDiv, currentDiv);
-  }
+    const slidesPerPageByScreenSize = {
+        0: 1,
+        768: 2,
+        1280: 3,
+    }
+
+    return Object.keys(slidesPerPageByScreenSize).reduce((acc, breakpoint) => {
+        if(width >= breakpoint) {
+            acc = slidesPerPageByScreenSize[breakpoint]
+        }
+        return acc
+    }, 1)
+}
+
+let cardsPerSlide = 1
+
+const initSlider = () => {
+    index = 0,
+    amount = 0,
+    currTransl = [],
+    translationComplete = true,
+    moveOffset = 0;
+    cardsPerSlide = calculateSliderPerPage(window.innerWidth)
+
+    const slider = document.querySelector('.our-friends__slider__cards')
+
+    slider.innerHTML = '';
+    
+    const cardsChunks = chunk(shuffledData, cardsPerSlide)
+    const slides = cardsChunks.map((slide, i) => {
+        if (slide.length < cardsPerSlide && i === cardsChunks.length - 1) {
+            const shouldAddXItems = cardsPerSlide - slide.length;
+
+            if(shouldAddXItems >= 1) {
+                return [...slide, shuffledData[0]]
+            }
+        }
+
+
+        return slide
+    })
+
+    console.log(slides)
+
+    slides.forEach((slide) => {
+        addSlide(slide)
+    })
+
+    var carousel = document.getElementById('carousel');
+
+    amount = document.getElementsByClassName("slide").length;
+
+
+
+    // get the width of the container
+    moveOffset = parseInt(window.getComputedStyle(document.getElementById('carousel-container')).width, 10);
+    console.log(moveOffset)
+    // calcuate the width of the carousel
+    carousel.style.width = (amount * moveOffset) + 'px';
+
+    // prevent multiple click when transition
+    for(var i = 0; i < amount; i++)
+    {
+        currTransl[i] = -moveOffset;
+        document.getElementsByClassName("slide")[i].addEventListener("transitionend", transitionCompleted, true);                    
+        document.getElementsByClassName("slide")[i].addEventListener("webkitTransitionEnd", transitionCompleted, true);                    
+        document.getElementsByClassName("slide")[i].addEventListener("oTransitionEnd", transitionCompleted, true);                    
+        document.getElementsByClassName("slide")[i].addEventListener("MSTransitionEnd", transitionCompleted, true);    
+        
+        document.getElementsByClassName('slide')[i].style.width = moveOffset + 'px';
+    }
+    // add the last item to the start so that translateX(-moveOffset) works (In case the first click is the previous button)
+    document.getElementById('carousel').insertBefore(document.getElementById('carousel').lastChild, document.getElementById('carousel').children[0])
+    
+}
+
+window.addEventListener('resize', function(event) {
+    const shouldReinitSlider = calculateSliderPerPage(window.innerWidth) !== cardsPerSlide
+
+    if(shouldReinitSlider) {
+        initSlider()
+    }
+}, true);
+
+document.addEventListener("DOMContentLoaded", function(event) 
+{
+    initSlider()
+    // add click events to control arrows
+    document.getElementById('prev').addEventListener('click', prev, true);
+    document.getElementById('next').addEventListener('click', next, true);
+});
+
+function prev()
+{
+    if (translationComplete === true)
+    {
+        translationComplete = false;
+        index--;
+        if (index == -1)
+        {
+            index = amount-1;
+        }
+        var outerIndex = (index) % amount;
+        for (var i = 0; i < amount; i++)
+        {
+            var slide = document.getElementsByClassName("slide")[i];
+            slide.style.opacity = '1';    
+            slide.style.transform = 'translateX('+(currTransl[i]+moveOffset)+'px)';
+            currTransl[i] = currTransl[i]+moveOffset;
+            console.log(currTransl[i], moveOffset)
+        }
+        var outerSlide = document.getElementsByClassName("slide")[outerIndex];
+        outerSlide.style.transform = 'translateX('+(currTransl[outerIndex]-(moveOffset*amount))+'px)';
+        outerSlide.style.opacity = '0';
+        currTransl[outerIndex] = currTransl[outerIndex]-moveOffset*(amount);
+    }
+}
+
+function next()
+{
+    if (translationComplete === true)
+    {
+        translationComplete = false;
+        var outerIndex = (index) % amount;
+        index++;
+        for(var i = 0; i < amount; i++)
+        {
+            var slide = document.getElementsByClassName("slide")[i];    
+            slide.style.opacity = '1';    
+            slide.style.transform = 'translateX('+(currTransl[i]-moveOffset)+'px)';
+            currTransl[i] = currTransl[i]-moveOffset;
+        }
+        var outerSlide = document.getElementsByClassName("slide")[outerIndex];
+        outerSlide.style.transform = 'translateX('+(currTransl[outerIndex]+(moveOffset*amount))+'px)';
+        outerSlide.style.opacity = '0';
+        currTransl[outerIndex] = currTransl[outerIndex]+moveOffset*(amount);
+    }
+}
