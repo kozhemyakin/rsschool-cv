249bb80f7794080fc0b15b961a5fa084c3caf49f
kozhemyakin <pctdoc@gmail.com>
2023-06-12 17:56:16 +0300

    fix: add ts to news file

---
+++ news-js-main/src/components/view/news/news.ts
@@ -0,0 +1,51 @@
+import './news.css';
+
+interface NewsItem {
+    author?: string;
+    description: string;
+    publishedAt: string;
+    source: {
+        id?: string;
+        name: string;
+    };
+    title: string;
+    url: string;
+    urlToImage?: string;
+}
+
+class News {
+    draw(data: NewsItem[]): void {
+        const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+
+        const fragment = document.createDocumentFragment();
+        const newsItemTemp = document.querySelector<HTMLTemplateElement>('#newsItemTemp');
+
+        news.forEach((item, idx) => {
+            const newsClone = newsItemTemp.content.cloneNode(true);
+
+            if (idx % 2) newsClone.querySelector('.news__item')!.classList.add('alt');
+
+            newsClone.querySelector('.news__meta-photo')!.style.backgroundImage = url(${
+                item.urlToImage || 'img/news_placeholder.jpg'
+            });
+            newsClone.querySelector('.news__meta-author')!.textContent = item.author || item.source.name;
+            newsClone.querySelector('.news__meta-date')!.textContent = item.publishedAt
+                .slice(0, 10)
+                .split('-')
+                .reverse()
+                .join('-');
+
+            newsClone.querySelector('.news__description-title')!.textContent = item.title;
+            newsClone.querySelector('.news__description-source')!.textContent = item.source.name;
+            newsClone.querySelector('.news__description-content')!.textContent = item.description;
+            newsClone.querySelector('.news__read-more a')!.setAttribute('href', item.url);
+
+            fragment.append(newsClone);
+        });
+
+        document.querySelector('.news')!.innerHTML = '';
+        document.querySelector('.news')!.appendChild(fragment);
+    }
+}
+
+export default News;
