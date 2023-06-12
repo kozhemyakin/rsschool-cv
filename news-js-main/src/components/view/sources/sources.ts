c43f922daf0bc89bbd2d7c487533dc66a614f521
kozhemyakin <pctdoc@gmail.com>
2023-06-12 17:51:04 +0300

    feat: add typescript and fix index and sources

---
+++ news-js-main/src/components/view/sources/sources.ts
@@ -0,0 +1,26 @@
+import './sources.css';
+
+interface Source {
+    id: number;
+    name: string;
+}
+
+class Sources {
+    draw(data: Source[]): void {
+        const fragment = document.createDocumentFragment();
+        const sourceItemTemp = document.querySelector<HTMLTemplateElement>('#sourceItemTemp');
+
+        data.forEach((item) => {
+            const sourceClone = sourceItemTemp.content.cloneNode(true);
+
+            sourceClone.querySelector('.source__item-name')!.textContent = item.name;
+            sourceClone.querySelector('.source__item')!.setAttribute('data-source-id', item.id.toString());
+
+            fragment.append(sourceClone);
+        });
+
+        document.querySelector('.sources')!.append(fragment);
+    }
+}
+
+export default Sources;
